b5601df426f873f860513cfdd4897dd7e5f39281
Aliaksandr Valadzko <Aliaksandr_Valadzko@epam.com>
2017-11-20 13:58:46 +0300

    failing tests

--- src/test/java/LogicTests.java
+++ src/test/java/LogicTests.java
@@ -11,4 +11,13 @@ public class LogicTests
 		String actualText = logic.printLogic();
 		Assert.assertEquals(actualText, expectedText);
 	}
+	
+	@Test
+	public void printLogicFailingTest()
+	{
+		Logic logic = new Logic();
+		String unexpectedText = "not logic text";
+		String actualText = logic.printLogic();
+		Assert.assertEquals(actualText, unexpectedText);
+	}
 }
